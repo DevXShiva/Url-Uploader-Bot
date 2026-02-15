f53ec4946a21709d8869391a82d636c12ddd861d
VoidXDev <developershiva18@gmail.com>
2026-02-15 12:43:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    # Ye page Render check karega health status ke liye
     return 'Bot is Running Securely!'
 
-def run_flask():
-    # Render automatically PORT environment variable deta hai
+# Iska naam 'run_server' hona chahiye taaki bot.py ise dhoond sake
+def run_server():
+    # Render default port 10000 use karta hai
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    run_server()
