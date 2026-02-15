d5629e9c260c93b10305b63b42def6682f9d8ae5
VoidXDev <developershiva18@gmail.com>
2026-02-15 13:41:51 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -1,5 +1,3 @@
-# @Shrimadhav Uk | @LISA_FAN_LK
-
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -23,22 +21,20 @@ from hachoir.parser import createParser
 from PIL import Image
 from pyrogram import enums 
 
-
-
 async def ddl_call_back(bot, update):
     logger.info(update)
     cb_data = update.data
     # youtube_dl extractors
     tg_send_type, youtube_dl_format, youtube_dl_ext = cb_data.split("=")
-    thumb_image_path = Config.DOWNLOAD_LOCATION + \
-        "/" + str(update.from_user.id) + ".jpg"
+    
     youtube_dl_url = update.message.reply_to_message.text
     custom_file_name = os.path.basename(youtube_dl_url)
+    
     if "|" in youtube_dl_url:
         url_parts = youtube_dl_url.split("|")
         if len(url_parts) == 2:
-            youtube_dl_url = url_parts[0]
-            custom_file_name = url_parts[1]
+            youtube_dl_url = url_parts[0].strip()
+            custom_file_name = url_parts[1].strip()
         else:
             for entity in update.message.reply_to_message.entities:
                 if entity.type == "text_link":
@@ -47,13 +43,6 @@ async def ddl_call_back(bot, update):
                     o = entity.offset
                     l = entity.length
                     youtube_dl_url = youtube_dl_url[o:o + l]
-        if youtube_dl_url is not None:
-            youtube_dl_url = youtube_dl_url.strip()
-        if custom_file_name is not None:
-            custom_file_name = custom_file_name.strip()
-        # https://stackoverflow.com/a/761825/4723940
-        logger.info(youtube_dl_url)
-        logger.info(custom_file_name)
     else:
         for entity in update.message.reply_to_message.entities:
             if entity.type == "text_link":
@@ -62,29 +51,40 @@ async def ddl_call_back(bot, update):
                 o = entity.offset
                 l = entity.length
                 youtube_dl_url = youtube_dl_url[o:o + l]
+
+    if youtube_dl_url is not None:
+        youtube_dl_url = youtube_dl_url.strip()
+    if custom_file_name is not None:
+        custom_file_name = custom_file_name.strip()
+
+    # Task ID for Cancel Logic
+    task_id = str(update.message.id)
+    if task_id not in Config.DOWNLOAD_LOCATION:
+        Config.DOWNLOAD_LOCATION.append(task_id)
+
     description = Translation.CUSTOM_CAPTION_UL_FILE
     start = datetime.now()
+    
     await update.message.edit_caption(
         caption=Translation.DOWNLOAD_START,
         parse_mode=enums.ParseMode.HTML
     )
-    tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+
+    tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION[0] + "/" + str(update.from_user.id)
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
     download_directory = tmp_directory_for_each_user + "/" + custom_file_name
-    command_to_exec = []
+
     async with aiohttp.ClientSession() as session:
         c_time = time.time()
         try:
-            await download_coroutine(
-                bot,
-                session,
-                youtube_dl_url,
-                download_directory,
-                update.message.chat.id,
-                update.message.id,
-                c_time
+            # Added task_id for cancellation support
+            download_status = await download_coroutine(
+                bot, session, youtube_dl_url, download_directory,
+                update.message.chat.id, update.message.id, c_time, task_id
             )
+            if download_status is False:
+                return False
         except asyncio.TimeoutError:
             await bot.edit_message_text(
                 text=Translation.SLOW_URL_DECED,
@@ -92,27 +92,21 @@ async def ddl_call_back(bot, update):
                 message_id=update.message.id
             )
             return False
+
     if os.path.exists(download_directory):
         end_one = datetime.now()
         await update.message.edit_caption(
             caption=Translation.UPLOAD_START,
             parse_mode=enums.ParseMode.HTML
         )
-        file_size = Config.TG_MAX_FILE_SIZE + 1
-        try:
-            file_size = os.stat(download_directory).st_size
-        except FileNotFoundError as exc:
-            download_directory = os.path.splitext(download_directory)[0] + "." + "mkv"
-            # https://stackoverflow.com/a/678242/4723940
-            file_size = os.stat(download_directory).st_size
+        
+        file_size = os.stat(download_directory).st_size
         if file_size > Config.TG_MAX_FILE_SIZE:
             await update.message.edit_caption(
-                
                 caption=Translation.RCHD_TG_API_LIMIT,
                 parse_mode=enums.ParseMode.HTML
             )
         else:
-            
             start_time = time.time()
             if (await db.get_upload_as_doc(update.from_user.id)) is False:
                 thumbnail = await Gthumb01(bot, update)
@@ -122,16 +116,12 @@ async def ddl_call_back(bot, update):
                     caption=description,
                     parse_mode=enums.ParseMode.HTML,
                     progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
-                    )
+                    progress_args=(Translation.UPLOAD_START, update.message, start_time)
                 )
             else:
-                 width, height, duration = await Mdata01(download_directory)
-                 thumb_image_path = await Gthumb02(bot, update, duration, download_directory)
-                 await update.message.reply_video(
+                width, height, duration = await Mdata01(download_directory)
+                thumb_image_path = await Gthumb02(bot, update, duration, download_directory)
+                await update.message.reply_video(
                     video=download_directory,
                     caption=description,
                     duration=duration,
@@ -141,27 +131,20 @@ async def ddl_call_back(bot, update):
                     parse_mode=enums.ParseMode.HTML,
                     thumb=thumb_image_path,
                     progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
-                    )
+                    progress_args=(Translation.UPLOAD_START, update.message, start_time)
                 )
+
+            # Handling Audio and VM types
             if tg_send_type == "audio":
                 duration = await Mdata03(download_directory)
                 thumbnail = await Gthumb01(bot, update)
                 await update.message.reply_audio(
                     audio=download_directory,
                     caption=description,
-                    parse_mode=enums.ParseMode.HTML,
                     duration=duration,
                     thumb=thumbnail,
                     progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
-                    )
+                    progress_args=(Translation.UPLOAD_START, update.message, start_time)
                 )
             elif tg_send_type == "vm":
                 width, duration = await Mdata02(download_directory)
@@ -172,25 +155,25 @@ async def ddl_call_back(bot, update):
                     length=width,
                     thumb=thumbnail,
                     progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
-                    )
+                    progress_args=(Translation.UPLOAD_START, update.message, start_time)
                 )
-            else:
-                logger.info("Did this happen? :\\")
+
             end_two = datetime.now()
+            # Cleanup Task ID
+            if task_id in Config.DOWNLOAD_LOCATION:
+                Config.DOWNLOAD_LOCATION.remove(task_id)
+
             try:
                 os.remove(download_directory)
-                os.remove(thumb_image_path)
+                if os.path.exists(thumb_image_path):
+                    os.remove(thumb_image_path)
             except:
                 pass
+
             time_taken_for_download = (end_one - start).seconds
             time_taken_for_upload = (end_two - end_one).seconds
             await update.message.edit_caption(
                 caption=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS.format(time_taken_for_download, time_taken_for_upload),
-               
                 parse_mode=enums.ParseMode.HTML
             )
     else:
@@ -199,56 +182,57 @@ async def ddl_call_back(bot, update):
             parse_mode=enums.ParseMode.HTML
         )
 
-async def download_coroutine(bot, session, url, file_name, chat_id, message_id, start):
+async def download_coroutine(bot, session, url, file_name, chat_id, message_id, start, task_id):
     downloaded = 0
     display_message = ""
     async with session.get(url, timeout=Config.PROCESS_MAX_TIMEOUT) as response:
-        total_length = int(response.headers["Content-Length"])
-        content_type = response.headers["Content-Type"]
+        total_length = int(response.headers.get("Content-Length", 0))
+        content_type = response.headers.get("Content-Type", "")
+        
         if "text" in content_type and total_length < 500:
             return await response.release()
-        await bot.edit_message_text(
-            chat_id,
-            message_id,
-            text="""Initiating Download
-URL: {}
-File Size: {}""".format(url, humanbytes(total_length))
-        )
+
         with open(file_name, "wb") as f_handle:
             while True:
+                # --- CRITICAL CANCEL CHECK ---
+                if task_id not in Config.DOWNLOAD_LOCATION:
+                    logger.info("Download cancelled by user.")
+                    return False
+
                 chunk = await response.content.read(Config.CHUNK_SIZE)
                 if not chunk:
                     break
                 f_handle.write(chunk)
-                downloaded += Config.CHUNK_SIZE
+                downloaded += len(chunk)
+                
                 now = time.time()
                 diff = now - start
-                if round(diff % 5.00) == 0 or downloaded == total_length:
+                if round(diff % 4.00) == 0 or downloaded == total_length:
                     percentage = downloaded * 100 / total_length
                     speed = downloaded / diff
                     elapsed_time = round(diff) * 1000
-                    time_to_completion = round(
-                        (total_length - downloaded) / speed) * 1000
-                    estimated_total_time = elapsed_time + time_to_completion
-                    try:
-                        current_message = """**Download Status**
-URL: {}
-File Size: {}
-Downloaded: {}
-ETA: {}""".format(
-    url,
-    humanbytes(total_length),
-    humanbytes(downloaded),
-    TimeFormatter(estimated_total_time)
-)
-                        if current_message != display_message:
-                            await bot.edit_message_text(
-                                chat_id,
-                                message_id,
-                                text=current_message
-                            )
+                    eta = round((total_length - downloaded) / speed) * 1000 if speed > 0 else 0
+                    
+                    # Animated Progress Bar
+                    progress_bar = "".join(["▰" for i in range(math.floor(percentage / 5))])
+                    remaining_bar = "".join(["▱" for i in range(20 - math.floor(percentage / 5))])
+                    
+                    current_message = (
+                        f"📥 **Dᴏᴡɴʟᴏᴀᴅɪɴɢ Fɪʟᴇ...**\n"
+                        f"┏━━━━━━━━━━━━━━━━━━━┓\n"
+                        f"┣ [{progress_bar}{remaining_bar}]\n"
+                        f"┣📦 **Pʀᴏɢʀᴇss :** {round(percentage, 2)}%\n"
+                        f"┣✅ **Dᴏɴᴇ :** {humanbytes(downloaded)}\n"
+                        f"┣📁 **Tᴏᴛᴀʟ :** {humanbytes(total_length)}\n"
+                        f"┣⚡ **Sᴘᴇᴇᴅ :** {humanbytes(speed)}/s\n"
+                        f"┣🕒 **ETA :** {TimeFormatter(eta)}\n"
+                        f"┗━━━━━━━━━━━━━━━━━━━┛"
+                    )
+
+                    if current_message != display_message:
+                        try:
+                            await bot.edit_message_text(chat_id, message_id, text=current_message)
                             display_message = current_message
-                    except Exception as e:
-                        logger.info(str(e))
-                        pass
-        return await response.release()
+                        except:
+                            pass
+        return True
